4bc7730f2cd099c7049863bba00979f32a3a2b5b
Emma Paris <autintristan@outlook.fr>
2021-02-15 15:21:35 +0100

    Modification du composant SearchBar

--- src/WeatherApp/WeatherApp.jsx
+++ src/WeatherApp/WeatherApp.jsx
@@ -1,38 +1,31 @@
 import React, { useState } from 'react'
 import DateBuilder from './DateBuilder';
+import SearchBar from './SearchBar';
 
 const api = {
-    key: "SECRET",
+    key: "64f721e6b055f85f93db14042305834d",
     base: "http://api.openweathermap.org/data/2.5/"
   }
 
 function WeatherApp(){
 
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
+const [weather, setWeather] = useState({});
 
-  const search = evt => {
-    if (evt.key === "Enter") {
+
+  const search = query => {
+
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
-          setQuery('');
           console.log(result);
         });
-    }
   }
+  
     return(
  <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
+        <SearchBar search={search}/>
         {(typeof weather.main != "undefined") ? (
         <div>
             <div className="location">{weather.name}, {weather.sys.country}</div>
