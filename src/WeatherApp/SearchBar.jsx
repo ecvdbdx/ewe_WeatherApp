a7c28f9b3f6b2340df0d7a9a4376645f257d634a
Emma Paris <autintristan@outlook.fr>
2021-02-15 09:50:57 +0100

    Ajout de la searchBar

---
+++ src/WeatherApp/SearchBar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import DateBuilder from './DateBuilder';
+
+const api = {
+    key: "SECRET",
+    base: "http://api.openweathermap.org/data/2.5/"
+  }
+
+
+
+function SearchBar(){
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+    return(
+ <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        {(typeof weather.main != "undefined") ? (
+        <div>
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date"><DateBuilder/></div>
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+        </div>
+        ) : ('')}
+      </main>
+    </div>
+    );
+}
+
+export default SearchBar;
