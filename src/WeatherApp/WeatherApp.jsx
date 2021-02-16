91d5b0bbc607f771eed31a1e14e5fb1c99a43124
Emma Paris <autintristan@outlook.fr>
2021-02-16 10:50:37 +0100

    Ajout de l'humidité et de la vitesse du vent

--- src/WeatherApp/WeatherApp.jsx
+++ src/WeatherApp/WeatherApp.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import DateBuilder from './DateBuilder';
 import SearchBar from './SearchBar';
 
+import './WeatherApp.css';
+
 const api = {
     key: "64f721e6b055f85f93db14042305834d",
     base: "http://api.openweathermap.org/data/2.5/"
@@ -14,7 +16,7 @@ const [weather, setWeather] = useState({});
 
   const search = query => {
 
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=metric&lang=fr&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
@@ -23,22 +25,34 @@ const [weather, setWeather] = useState({});
   }
   
     return(
- <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
-        <SearchBar search={search}/>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date"><DateBuilder/></div>
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
+      <SearchBar search={search}/>
+      {(weather.main) ? (
+        <section>
+          <div className="weather-side">
+            <div className="date-location">
+              <DateBuilder/>
+              <div className="location">{weather.name}, {weather.sys.country}</div>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
-            <img src={"http://openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png"}></img>
-        </div>
-        ) : ('')}
+            <div className="weather-temp">
+              <img className="weather-img" src={"http://openweathermap.org/img/wn/" + weather.weather[0].icon + "@2x.png"}></img>
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+          <div className="info-side">
+            <div className="humidity">
+              <span>humidity</span>
+              <span>{weather.main.humidity}%</span>
+            </div>
+            <div className="humidity">
+              <span>wind</span>
+              <span className="wind">{weather.wind.speed} km/h</span>
+            </div>
+          </div>
+        </section>
+      ) : null}
       </main>
-    </div>
     );
 }
 
